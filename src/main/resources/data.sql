51c0e7392785adb814c16a040b1ade94d8a91107
acmartinr <albertocarlosmartin@gmail.com>
2022-10-31 01:55:32 +0100

    added sql for insert internal DB tables

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO SUPER_HERO (name,occupation,publisher,race,gender) VALUES ('Super Man','journalist','dc','human','male');
+INSERT INTO SUPER_HERO (name,occupation,publisher,race,gender) VALUES ('Spider Man','engineer','marvel','human','male');
+INSERT INTO SUPER_HERO (name,occupation,publisher,race,gender) VALUES ('Black Widow','spy','marvel','human','female');
+INSERT INTO SUPER_HERO (name,occupation,publisher,race,gender) VALUES ('Batman','detective','dc','human','male');
