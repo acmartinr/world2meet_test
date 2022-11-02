ceeaba411fd399bddd550d733cd2379bd6b19738
acmartinr <albertocarlosmartin@gmail.com>
2022-11-02 01:57:42 +0100

    removed unused dependency

--- src/main/java/com/world2meet/test/security/SecurityConfiguration.java
+++ src/main/java/com/world2meet/test/security/SecurityConfiguration.java
@@ -1,9 +1,7 @@
 package com.world2meet.test.security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,15 +11,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import java.time.Duration;
 import java.util.Arrays;
-
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    //url list
     private static final String[] AUTH_LIST = {
             "/v2/api-docs",
             "/configuration/ui",
