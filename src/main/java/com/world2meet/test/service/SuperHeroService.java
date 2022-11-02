b1c87b9b8ca137f7bc1de97237287f876f32f385
acmartinr <albertocarlosmartin@gmail.com>
2022-11-02 01:58:09 +0100

    added update and dinfbyId service

--- src/main/java/com/world2meet/test/service/SuperHeroService.java
+++ src/main/java/com/world2meet/test/service/SuperHeroService.java
@@ -1,10 +1,12 @@
 package com.world2meet.test.service;
 
+import com.world2meet.test.payload.request.SuperHeroRequest;
 import com.world2meet.test.persistence.model.SuperHero;
 import com.world2meet.test.persistence.repository.SuperHeroRepository;
 import com.world2meet.test.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -19,12 +21,23 @@ public class SuperHeroService {
         return new ArrayList<>(superHeroRepository.findAll());
     }
 
-    //getting all prices records
+    //get all super hery by id
     public Optional<SuperHero> getSuperHeroById(Long id) {
         return superHeroRepository.findById(id);
     }
 
+    //update super hero with id in param and all fields to update
+    public Optional<SuperHero> updateSuperHeroById(Long id, SuperHero superHeroModel) {
+        superHeroModel.setId(id);
+        SuperHero superHero = superHeroRepository.save(superHeroModel);
+        Optional<SuperHero> userOptional = Optional.of(superHero);
+        return userOptional;
+    }
 
+    //getting all super heroes records
+    public List<SuperHero> findSuperHeroesByName(String name) {
+        return new ArrayList<>(superHeroRepository.filterSuperHeroByName(name));
+    }
 
     //getting all prices records
     public int deleteSuperHeroById(Long id) {
@@ -34,11 +47,6 @@ public class SuperHeroService {
         } else {
             return Constants.NOT_SUPER_HEROES_FOUND_CODE;
         }
-    }
-
-    //get super hero that match name
-    public List<SuperHero> getSuperHeroesByName(String name) {
-        return superHeroRepository.filterSuperHeroByName(name);
     }
 
 }
