2e11b41b555ce2521da5ec971034b17013b6b0ac
acmartinr <albertocarlosmartin@gmail.com>
2022-11-03 11:09:35 +0100

    added methods for trace response time

---
+++ src/main/java/com/world2meet/test/advice/TrackExecutionTime.java
@@ -0,0 +1,11 @@
+package com.world2meet.test.advice;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TrackExecutionTime {
+
+}
